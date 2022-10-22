afa4dfe8b7b70c0cfbd2792d55bb334b2e533279
Maciej <maciejdrobnik34@gmail.com>
2022-10-22 14:54:46 +0200

    ngx Quill configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import {QuillModule} from 'ngx-quill'
+import {FormsModule} from "@angular/forms";
+import { TextEditorComponent } from './text-editor/text-editor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TextEditorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    QuillModule.forRoot(),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
