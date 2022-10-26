5c0b8b67eef6b80c3a4b7ae612ceedf26249f070
Aizu4 <234679@edu.p.lodz.pl>
2022-10-26 13:06:44 +0200

    Added router and made the components for the main page and a page from the backend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,32 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatToolbarModule} from "@angular/material/toolbar";
 import {MatIconModule} from "@angular/material/icon";
 import {MatButtonModule} from "@angular/material/button";
 import {MatSidenavModule} from "@angular/material/sidenav";
 import {MatTreeModule} from "@angular/material/tree";
 import {MatInputModule} from "@angular/material/input";
-import { ToolbarComponent } from './toolbar/toolbar.component';
-import { DrawerComponent } from './drawer/drawer.component';
+import {ToolbarComponent} from './toolbar/toolbar.component';
+import {DrawerComponent} from './drawer/drawer.component';
+import {RouterModule, Routes} from "@angular/router";
+import { HomeComponent } from './pages/home/home.component';
+import { PageComponent } from './pages/page/page.component';
+
+const appRoutes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: ':id', component: PageComponent},
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     ToolbarComponent,
-    DrawerComponent
+    DrawerComponent,
+    HomeComponent,
+    PageComponent,
   ],
   imports: [
     BrowserModule,
@@ -26,9 +36,11 @@ import { DrawerComponent } from './drawer/drawer.component';
     MatButtonModule,
     MatSidenavModule,
     MatTreeModule,
-    MatInputModule
+    MatInputModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
