1ed4f14e0a3a9a18469d54cc3b44cf9915f2f5b4
jadvviga <ster.jadwiga@gmail.com>
2022-12-13 20:08:32 +0100

    url and languages suppport wip

--- src/app/pages/page/page.component.ts
+++ src/app/pages/page/page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import { PageService } from "../../services/page.service";
+import {Location} from "@angular/common";
 
 @Component({
   selector: 'app-page',
@@ -11,17 +12,22 @@ export class PageComponent implements OnInit {
 
   id: string;
   pageHTML: string = "";
+  pagePath: string;
 
   constructor(
     private route: ActivatedRoute,
-    private pageService: PageService
+    private pageService: PageService,
+    private location: Location
   ) {
   }
 
-
+//todo add 'check for latex' func on html
 
   ngOnInit(): void {
     this.id = this.route.snapshot.params['id'];
+    //will use later for path
+    //this.pagePath = '/test';
+    //this.location.replaceState(this.pagePath);
     this.route.params.subscribe(val => {
       this.id = this.route.snapshot.params['id'];
       this.getPage();
--- src/app/services/menu.service.ts
+++ src/app/services/menu.service.ts
@@ -2,17 +2,22 @@ import {Injectable} from '@angular/core';
 import { Page} from "./mock-menu-data";
 import {Observable, of} from "rxjs";
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import {LanguageService} from "./language.service";
 
 const URL = 'http://localhost:8080/';
+
 const PAGE_URL = URL + 'menu';
 @Injectable({
   providedIn: 'root'
 })
 export class MenuService {
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, private languageService: LanguageService) {
   }
 
   getMenuData(): Observable<Page[]> {
-    return this.http.get<Page[]>(PAGE_URL, {responseType: 'json'});
+    const lang = this.languageService.getLanguage();
+    const url = PAGE_URL + '/' + lang; //for now to test language change this part manually to eg
+   // const url = PAGE_URL + '/' + 'polish';
+    return this.http.get<Page[]>(url, {responseType: 'json'});
   }
 }
