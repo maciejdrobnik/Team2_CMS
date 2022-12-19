19704208401d318b78f7f9c00dca9f2e9865de5d
jadvviga <ster.jadwiga@gmail.com>
2022-12-19 13:59:18 +0100

    latex tag configuration

--- src/app/pages/page/page.component.ts
+++ src/app/pages/page/page.component.ts
@@ -1,7 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
-import { PageService } from "../../services/page.service";
+import {PageDTO, PageService} from "../../services/page.service";
 import {Location} from "@angular/common";
+
+
+interface PageContent {
+  latex: boolean,
+  content: string;
+}
+
 
 @Component({
   selector: 'app-page',
@@ -12,6 +19,7 @@ export class PageComponent implements OnInit {
 
   id: string;
   pageHTML: string = "";
+  pageContent: Array<PageContent> = [];
   pagePath: string;
 
   constructor(
@@ -35,9 +43,49 @@ export class PageComponent implements OnInit {
   }
 
   getPage(): void {
-    this.pageService.getPage(this.id).subscribe((page) => {
-      this.pageHTML = page.content;
+    this.pageService.getPage(this.id).subscribe( {
+      next: (page: PageDTO) => {this.pageHTML = page.content;},
+      error: () => {},
+      complete: () => {
+        this.separateContent();
+      }
     });
+  }
+
+
+  checkForLatex(): number {
+    const match = this.pageHTML.match('<latex>');
+    return match?.length || 0;
+  }
+
+  separateContent() {
+    if (this.checkForLatex() !== 0) {
+      const tag = '<latex>';
+      const endTag = '</latex>';
+      const content = this.pageHTML.split(tag);
+      console.log(content)
+      content.forEach(str => {
+        if(str.includes(endTag)) {
+          const separated = str.split(endTag);
+          const ltx = separated[0];
+          const normal = separated[1];
+          this.pageContent.push({latex: true, content: ltx});
+          if(normal.length !== 0) {
+            this.pageContent.push({latex: false, content: normal});
+          }
+
+        } else {
+          if(str.length !== 0) {
+            this.pageContent.push({latex: false, content: str});
+          }
+
+        }
+      });
+      console.log(this.pageContent);
+
+    } else {
+      this.pageContent.push({latex: false, content: this.pageHTML});
+    }
   }
 
 
